12cb6312af09e2f1e1dc320a55921725e496eda2
Leroy aurelien <83220559+aurelienLRY@users.noreply.github.com>
2024-12-29 23:19:07 +0100

    Branding (#69)
    * Update favicon and refactor layout components for branding enhancement
    
    - Updated the favicon to reflect the new branding.
    - Changed the layout metadata title and description to "Easylis | Gestionnaire d'activités pleine nature".
    - Enhanced the Header component by integrating an image logo for better visual representation.
    
    These changes improve the overall branding and user interface of the application.

--- src/components/layout/Header.layout.tsx
+++ src/components/layout/Header.layout.tsx
@@ -3,6 +3,7 @@
 /* Components */
 import { HeaderBtn } from "@/components";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 /**
  * Header Component
@@ -12,9 +13,18 @@ export const Header = () => {
   const { status } = useSession();
   return (
     <header className="flex justify-between items-center p-3  lg:px-10 md:fixed top-0 w-full z-40 bg-slate-900 dark:bg-sky-950 text-white bg-opacity-60 backdrop-blur-md">
-      <h1 className="text-4xl font-bold">
-        {process.env.NEXT_PUBLIC_BRANDING_NAME || "Easylis"}
-      </h1>
+      <div className="flex items-center gap-2">
+        <Image
+          src="/img/Easylis.png"
+          alt="logo"
+          width={100}
+          height={100}
+          className="w-10 h-10 "
+        />
+        <h1 className="text-4xl ">
+          {process.env.NEXT_PUBLIC_BRANDING_NAME || "Easylis"}
+        </h1>
+      </div>
       <div className="flex items-center gap-2">
         {status === "authenticated" && <HeaderBtn />}
       </div>
