80e58d2c105a8d72ac0d5b5f347088856443d926
Leroy aurelien <leroyaurelien11@gmail.com>
2025-06-26 13:46:06 +0200

    Ajout de la structure de données pour les sessions et les spots dans les routes API
    - Intégration de nouveaux types de données pour les sessions et les spots, incluant des détails sur les activités et les lieux.
    - Mise à jour de la route des sessions pour renvoyer les sessions actives avec les informations d'activités et de spots associées.
    - Révision de la route des spots pour inclure un type de données structuré dans la réponse.
    
    Ces modifications améliorent la clarté des données renvoyées par les API et facilitent l'intégration des informations liées aux sessions et aux spots.

--- src/app/api/services/external/sessions/route.ts
+++ src/app/api/services/external/sessions/route.ts
@@ -1,7 +1,68 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { connectDB, disconnectDB } from "@/libs/database/setting.mongoose";
-import { Session } from "@/libs/database";
+import { Session , Activity , Spot } from "@/libs/database";
+
+
+ const Tsession = {
+ 
+        "_id": "Types.ObjectId",
+        "status": "string",
+        "date": "Date",
+        "startTime": "string",
+        "endTime": "string",
+        "activity": {
+            "price_half_day": {
+                "standard": "number",
+                "reduced": "number",
+                "ACM": "number"
+            },
+            "price_full_day": {
+                "standard": "number",
+                "reduced": "number",
+                "ACM": "number"
+            },
+            "duration": {
+                "half": " string | null",
+                "full": " string | null"
+            },
+            "_id": " Types.ObjectId",
+            "name": " string",
+            "description": " string",
+            "half_day": " boolean",
+            "full_day": " boolean",
+            "min_age": " number",
+            "max_OfPeople": " number",
+            "min_OfPeople": " number",
+            "required_equipment": " string",
+            "__v": " number"
+        },
+        "spot": {
+            "meetingPoint": {
+                "half_day": " string | null",
+                "full_day": " string | null"
+            },
+            "_id": " Types.ObjectId",
+            "name": " string",
+            "description": " string",
+            "practicedActivities": [
+                {
+                    "activityName": " string",
+                    "activityId": " Types.ObjectId",
+                    "_id": " Types.ObjectId"
+                }
+            ],
+            "photo": " string",
+            "gpsCoordinates": " string",
+            "__v": " number"
+        },
+        "placesMax": " number",
+        "placesReserved": " number",
+        "type_formule": " string",
+        "duration": " string",
+        "__v": " number"
+    }
+
 
 
 export async function GET(req: NextRequest) {
@@ -12,7 +73,23 @@ export async function GET(req: NextRequest) {
       date: { $gte: new Date() },
       status: "Actif"
     });
-    return NextResponse.json(sessions, { status: 200 });
+    const activities = await Activity.find({});
+    const spots = await Spot.find({});
+
+    const sessionsWithActivitiesAndSpots = sessions.map((session) => {
+      const activity = activities.find((activity) => activity._id.toString() === session.activity.toString());
+      const spot = spots.find((spot) => spot._id.toString() === session.spot.toString());
+      return {
+        ...session.toObject(),
+        activity: activity?.toObject(),
+        spot: spot?.toObject()
+      }
+    });
+
+    return NextResponse.json({ 
+      Type : Tsession,
+      ActiveSessions: sessionsWithActivitiesAndSpots 
+    }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
