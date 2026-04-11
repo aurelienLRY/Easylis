39793568e7b8326ad0fc33f6437f2d6c8ee4d5a8
Leroy aurelien <leroyaurelien11@gmail.com>
2026-04-11 12:38:19 +0200

    Update version to 0.11.0 and enhance layout structure
    - Updated package.json and package-lock.json to version 0.11.0.
    - Refactored layout.tsx to include Header and Footer components, improving the overall structure of the application.
    - Added new client dashboard page with session management features, including sorting and filtering capabilities for customer sessions.
    - Updated dashboard template to include a new case for client navigation.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ConfigProvider } from "antd";
 import { Providers } from "@/components/layout/Providers.layout";
+import { Header } from "@/components/layout/Header.layout";
+import { Footer } from "@/components/layout/Footer.layout";
 
 /*components */
 const inter = Inter({ subsets: ["latin"] });
@@ -53,7 +55,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="fr" className=" dark">
-      <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
+      <body className={`${inter.className} dark:bg-gray-900 dark:text-white flex flex-col`}>
         <ConfigProvider
           theme={{
             token: {
@@ -61,7 +63,13 @@ export default async function RootLayout({
             },
           }}
         >
-          <Providers>{children}</Providers>
+          <Providers>
+            <Header />
+            <main className="flex flex-col items-center justify-center  box-border mt-4 md:mt-16 scroll-smooth min-h-[89vh]">
+            {children}
+            </main>
+            <Footer />
+          </Providers>
         </ConfigProvider>
       </body>
     </html>
